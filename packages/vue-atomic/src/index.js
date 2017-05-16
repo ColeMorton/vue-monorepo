abc6efb70c5d08af490ec2ae3216586def4d3eb4
Cole Morton <cole.morton@hotmail.com>
2017-05-16 16:07:34 +0200

    Add sue loader and export Box

--- packages/vue-atomic/src/index.js
+++ packages/vue-atomic/src/index.js
@@ -1,12 +1,12 @@
-import vue from 'vue';
+import Box from './components/box/Box.vue';
 console.log('vue-atomic started');
 
 function test() {
   console.log('test');
-  console.log('vue loaded: ', vue)
   return true;
 }
 
 export default {
-  test: test
+  test: test,
+  Box: Box
 }
--- packages/vue-atomic/webpack.config.js
+++ packages/vue-atomic/webpack.config.js
@@ -12,6 +12,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
