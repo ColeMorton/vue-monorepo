6d55d4b678a96c3073ef676355c43ed3f1d64d02
Cole Morton <cole.morton@hotmail.com>
2017-05-16 23:35:43 +0200

    clean up

--- packages/vue-sandbox/packages/vue-atomic/src/index.js
+++
@@ -1,7 +0,0 @@
-const component = () => {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello Babel Latest!';
-  return element;
-}
-
-document.body.appendChild(component());
