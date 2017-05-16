eea244896495af0fb426e21bcb5ad86d9e46005f
Cole Morton <cole.morton@hotmail.com>
2017-05-16 12:53:00 +0200

    Add components

---
+++ packages/vue-sandbox/packages/vue-atomic/src/components/box/Box.play.js
@@ -0,0 +1,10 @@
+import {play} from 'vue-play'
+import Box from './Box.vue'
+
+play(Box)
+  .name('Box')
+  .displayName('The Box Component')
+  .add('default', '<Box />')
+  .add('dash border', '<Box :dashed="true" />')
+  .add('dotted border', '<Box :dotted="true" />')
+  .add('colorful', '<Box color="pink" />')
