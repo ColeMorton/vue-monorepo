49801ff7a726dee49881af191a641b0bc1fd84c7
Cole Morton <cole.morton@hotmail.com>
2017-05-20 14:03:03 +0200

    remove box explicit import

--- packages/vue-atomic/play/index.js
+++ packages/vue-atomic/play/index.js
@@ -1,5 +1,3 @@
-import boxPlay from '../src/components/box/Box.play.js'
-
 const load = requireContext => requireContext.keys().map(requireContext)
 
 // load files which end with `.play.js` in `../src/components` folder
