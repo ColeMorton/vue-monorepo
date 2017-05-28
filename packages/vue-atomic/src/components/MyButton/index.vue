853377e940cc89e412dd25a838022cef6dee2f12
Cole Morton <cole.morton@hotmail.com>
2017-05-28 10:21:58 +0200

    remove test component

--- packages/vue-atomic/src/components/MyButton/index.vue
+++ packages/vue-atomic/src/components/MyButton/index.vue
@@ -1,27 +1,52 @@
-<template>
-  <button class="button" :style="{color: color, borderColor: color}" @click="handleClick" :class="{rounded: rounded}"><slot></slot></button>
+<template lang="pug">
+button.button(
+  v-bind:style='{ color: color, borderColor: color, fontSize: fontSize }'
+  v-bind:class='{ rounded: rounded }'
+  @click='handleClick'
+)
+  slot
 </template>
 
 <script>
   export default {
     props: {
       rounded: Boolean,
-      handleClick: {
-        default: () => () => null
-      },
       color: {
         default: '#42b983'
+      },
+      palette: {
+        default: 'primary'
+      },
+      type: {
+        default: 'button'
+      },
+      height: {
+        default: 40
       }
+    },
+    computed: {
+      fontSize: function () {
+        return `${this.height / 40}rem`
+      }
+    },
+    methods: {
+      handleClick: () => console.log('clicked!'),
     }
   }
 </script>
 
-<style>
+<style scoped>
   .button {
-    border: 3px solid;
-    padding: 10px 20px;
-    border-radius: 3px;
-    background-color: white;
-    outline: none;
+    display: inline-flex;
+    align-items: center;
+    white-space: nowrap;
+    border: 0.0625em solid;
+    height: 2.5em;
+    justify-content: center;
+    text-decoration: none;
+    padding: 0 1em;
+    border-radius: 0.125em;
+    box-sizing: border-box;
+    transition: background-color 250ms ease-out, color 250ms ease-out, border-color 250ms ease-out;
   }
 </style>
